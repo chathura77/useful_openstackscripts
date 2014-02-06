41de6593170cea2767619778d8c5515d14e65c20
Chathura Sarathchandra <chathura@Chathuras-MacBook-Pro.local>
2014-02-06 12:50:03 +0000

    Changed email address

--- controller/fix_openvswitch_mod.sh
+++ controller/fix_openvswitch_mod.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # This script fixes following problem.
 
 # FATAL: Module openvswitch_mod not found.
@@ -5,10 +7,13 @@
 #  * not removing bridge module because bridges exist (virbr0)
 # invoke-rc.d: initscript openvswitch-switch, action "load-kmod" failed.
 
-sudo apt-get install linux-headers-$(uname -r) 
+sudo apt-get install linux-headers-$(uname -r)
 
-dpkg -r quantum-plugin-openvswitch-agent openvswitch-switch openvswitch-datapath-dkms openvswitch-common quantum-common python-quantum
+sudo apt-get remove quantum-plugin-openvswitch openvswitch-switch quantum-plugin-openvswitch-agent openvswitch-datapath-dkms openvswitch-common quantum-common python-quantum
 
 apt-get install openvswitch-switch
 mkdir -p /etc/quantum/
-apt-get install quantum-plugin-openvswitch-agent
+apt-get install quantum-plugin-openvswitch-agent quantum-dhcp-agent quantum-l3-agent quantum-server
+
+depmod
+modprobe openvswitch
